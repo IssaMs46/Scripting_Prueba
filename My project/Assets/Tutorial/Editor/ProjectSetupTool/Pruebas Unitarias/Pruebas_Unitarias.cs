5aa0d218dc777ea811e6e8b83238bc981a6ddf12
IssaMs46 <isams2004.1@gmail.com>
2024-04-06 10:33:23 -0500

    info introduccion pruebas unitarias

--- My project/Assets/Tutorial/Editor/ProjectSetupTool/Pruebas Unitarias/Pruebas_Unitarias.cs
+++ My project/Assets/Tutorial/Editor/ProjectSetupTool/Pruebas Unitarias/Pruebas_Unitarias.cs
@@ -9,15 +9,18 @@ namespace MiTutorial
     {
         
         static Pruebas_Unitarias win;
-        
+        private string QueEs = "Las pruebas unitarias son pequeñas pruebas automatizadas \nque se realizan en fragmentos individuales de código, \nconocidos como unidades. Estas unidades pueden \nser funciones, métodos o clases. Las pruebas \nunitarias se centran en evaluar el \ncomportamiento de una unidad de código de \nmanera aislada, es decir, sin depender de \notros componentes del sistema.";
+        private string ParaQue = "Las pruebas unitarias sirven para verificar que las \nunidades de código funcionan como se espera. Al automatizar \nestas pruebas, los desarrolladores pueden detectar \nerrores y problemas de lógica en el código de manera \ntemprana en el proceso de desarrollo. Esto permite \ncorregir los errores de manera oportuna y \ngarantizar que el software sea más confiable y robusto.\n Además, las pruebas unitarias proporcionan documentación \nviva del comportamiento esperado de cada unidad de código, \nlo que facilita el mantenimiento y la colaboración en proyectos \nde desarrollo de software.";
+
+        Vector2 scrollPosition = Vector2.zero;
 
         //private string gameName = "Game";
-        
 
-        
+
+
         public static void InitWindow ()
         {
-            win = EditorWindow.GetWindow<Pruebas_Unitarias>("Project Setup");
+            win = EditorWindow.GetWindow<Pruebas_Unitarias>("Pruebas Unitarias");
             
             win.Show();
         }
@@ -25,19 +28,51 @@ namespace MiTutorial
         void OnGUI()
         {
 
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Project Setup", EditorStyles.boldLabel);  // TÍTULO EN NEGRITA
-            EditorGUILayout.LabelField("Proyect Setup");  //TEXTO NORMAL
+            GUIStyle bigBoldLabelStyle = new GUIStyle(EditorStyles.boldLabel);
+            bigBoldLabelStyle.fontSize = 16;
 
-            //gameName=EditorGUILayout.TextField("Game name:", gameName);
-            EditorGUILayout.EndHorizontal();
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition); // Agregar scroll vertical general
 
-            //if(GUILayout.Button ("Create Project Structure", GUILayout.Height(30), GUILayout.ExpandWidth(true)))
-            //{
-            //    Debug.Log("presion de botón");
-            //}
 
-            if(win!= null) 
+            EditorGUILayout.LabelField("¿Qué son?", bigBoldLabelStyle, GUILayout.ExpandHeight(false));  // TÍTULO EN NEGRITA
+            
+
+            string[] QueEslines = QueEs.Split(new[] { "\n", "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in QueEslines)
+            {
+                EditorGUILayout.LabelField(line);
+            }
+
+
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("¿Para qué sirve?", bigBoldLabelStyle, GUILayout.ExpandHeight(false));  // TÍTULO EN NEGRITA
+
+
+
+            string[] ParaQuelines = ParaQue.Split(new[] { "\n", "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (string line in ParaQuelines)
+            {
+                EditorGUILayout.LabelField(line);
+            }
+
+            EditorGUILayout.Space();
+            GUIStyle linkStyle = new GUIStyle(GUI.skin.label);
+            linkStyle.normal.textColor = Color.blue;
+            if (GUILayout.Button("Más información", linkStyle))
+            {
+                // Abrir el enlace en el navegador
+                Application.OpenURL("https://learn.microsoft.com/es-es/visualstudio/test/unit-test-basics?view=vs-2022");
+            }
+
+            EditorGUILayout.EndScrollView();
+
+            //REVISAR SI USAR ESTE LINK O SI USAR OTRO
+
+            if (win!= null) 
             {
                 win.Repaint();
             }
